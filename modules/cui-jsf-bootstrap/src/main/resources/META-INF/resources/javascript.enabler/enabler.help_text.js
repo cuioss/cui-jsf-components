ce9d076c5ac8515b73d4aff70cf2384e43b67f47
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2024-04-01 18:41:59 +0200

    Fixing typo

--- modules/cui-jsf-bootstrap/src/main/resources/META-INF/resources/javascript.enabler/enabler.help_text.js
+++ modules/cui-jsf-bootstrap/src/main/resources/META-INF/resources/javascript.enabler/enabler.help_text.js
@@ -1,6 +1,6 @@
 /**
  * Used for initializing GuardedInput components.
- * The implementations assumes JQuery, jsf.js and cui.jsf being present.
+ * The implementation assumes JQuery, jsf.js and cui.jsf being present.
  */
 let intitializeHelpTexts = function () {
     jQuery('[data-help-input-button]').each(function () {
