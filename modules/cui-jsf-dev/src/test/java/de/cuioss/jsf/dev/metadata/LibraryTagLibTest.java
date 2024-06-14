0a7c29422caa3dcf0aba3fdbc0dc7fe6db5a31fd
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2024-06-14 16:30:08 +0200

    Fix Loading of taglibs, replace myfaces with component local mojarra

--- modules/cui-jsf-dev/src/test/java/de/cuioss/jsf/dev/metadata/LibraryTagLibTest.java
+++ modules/cui-jsf-dev/src/test/java/de/cuioss/jsf/dev/metadata/LibraryTagLibTest.java
@@ -2,23 +2,54 @@ package de.cuioss.jsf.dev.metadata;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTagLibTest {
 
     @Test
-    void shouldLoadTaglibs() {
-        for (var lib : LibraryTagLib.values()) {
-            var loaded = assertDoesNotThrow(lib::load, lib.name());
-            assertNotNull(loaded);
-        }
+    void shouldHandleFacesHtml() {
+        var loaded = LibraryTagLib.FACES_HTML.load();
+        assertNotNull(loaded);
+        var editableDataList = loaded.getComponentMetadata().getByName("inputHidden");
+        assertTrue(editableDataList.isPresent());
+    }
+
+
+    @Test
+    void shouldHandleFacesCore() {
+        var loaded = LibraryTagLib.FACES_CORE.load();
+        assertNotNull(loaded);
+        var tag = loaded.getConverterMetadata().getByName("convertDateTime");
+        assertTrue(tag.isPresent());
     }
 
     @Test
-    void shouldHandleMyFaces() {
-        var loaded = LibraryTagLib.MY_FACES.load();
+    void shouldHandleCuiBootstrap() {
+        var loaded = LibraryTagLib.CUI_BOOTSTRAP.load();
         assertNotNull(loaded);
-        System.out.println(loaded);
+        var tag = loaded.getComponentMetadata().getByName("editableDataList");
+        assertTrue(tag.isPresent());
+    }
+    @Test
+    void shouldHandleCuiCore() {
+        var loaded = LibraryTagLib.CUI_CORE.load();
+        assertNotNull(loaded);
+        var tag = loaded.getComponentMetadata().getByName("fieldset");
+        assertTrue(tag.isPresent());
+    }
+    @Test
+    void shouldHandleOmnifaces() {
+        var loaded = LibraryTagLib.OMNI_FACES.load();
+        assertNotNull(loaded);
+        var tag = loaded.getComponentMetadata().getByName("tree");
+        assertTrue(tag.isPresent());
+    }
+
+    @Test
+    void shouldHandlePrimefaces() {
+        var loaded = LibraryTagLib.PRIME_FACES.load();
+        assertNotNull(loaded);
+        var tag = loaded.getComponentMetadata().getByName("dataTable");
+        assertTrue(tag.isPresent());
     }
 }
