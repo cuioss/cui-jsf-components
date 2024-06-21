4ba2d0543c6410bf76c716f07486f9ca1dddc2b4
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2024-06-21 18:00:30 +0200

    Switching to latest parent

--- modules/cui-jsf-dev/src/main/java/de/cuioss/jsf/dev/metadata/LibraryTagLib.java
+++ modules/cui-jsf-dev/src/main/java/de/cuioss/jsf/dev/metadata/LibraryTagLib.java
@@ -4,8 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import static de.cuioss.jsf.dev.metadata.TagLib.JSF_2_FACELET_TAGLIB_NAMESPACE;
-import static de.cuioss.jsf.dev.metadata.TagLib.JSF_4_0_FACELET_TAGLIB_NAMESPACE;
+import static de.cuioss.jsf.dev.metadata.TagLib.*;
 
 /**
  * Isolates concrete {@link TagLib}s to be loaded.
@@ -15,7 +14,7 @@ public enum LibraryTagLib {
 
     CUI_CORE("/META-INF/cui-core.taglib.xml", JSF_4_0_FACELET_TAGLIB_NAMESPACE),
     CUI_BOOTSTRAP("/META-INF/cui-bootstrap.taglib.xml", JSF_4_0_FACELET_TAGLIB_NAMESPACE),
-    PRIME_FACES("/META-INF/primefaces-p.taglib.xml", JSF_2_FACELET_TAGLIB_NAMESPACE),
+    PRIME_FACES("/META-INF/primefaces.taglib.xml", JSF_2_2_FACELET_TAGLIB_NAMESPACE),
     OMNI_FACES("/META-INF/omnifaces-ui.taglib.xml", JSF_4_0_FACELET_TAGLIB_NAMESPACE),
     FACES_CORE("/META-INF/resources/mojarra-4.0.7-tlds/faces.core_taglib.xml", JSF_4_0_FACELET_TAGLIB_NAMESPACE),
     FACES_HTML("/META-INF/resources/mojarra-4.0.7-tlds/faces.html_taglib.xml", JSF_4_0_FACELET_TAGLIB_NAMESPACE);
